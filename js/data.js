8ca82cc85ee884be366e2edb75414d62b44c3520
calvin <CalCodez@outlook.com>
2024-12-24 15:42:57 -0600

    init project setup commit

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const date = new Date();
+
+const dateObj = {
+	fullYear: date.getFullYear(),
+	month: date.getMonth() + 1,
+	longMonthName: date.toLocaleString('default', { month: 'long' }),
+	shortMonthName: date.toLocaleString('default', { month: 'short' }),
+	day: date.getDate(),
+	longDayName: date.toLocaleString('default', { weekday: 'long' }),
+	shortDayName: date.toLocaleString('default', { weekday: 'short' }),
+	dayOfWeek: date.getDay() + 1,
+	fullDate1: date.toDateString(),
+	fullDate2: date.toLocaleDateString(),
+};
+
+const time = {
+	hours: date.getHours(),
+	minutes: date.getMinutes(),
+	seconds: date.getSeconds(),
+};
