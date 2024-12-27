176026a50b19cd8c1f208c80be2adb50204f4c80
calvin <CalCodez@outlook.com>
2024-12-27 14:59:04 -0600

    settings defaults

--- js/data.js
+++ js/data.js
@@ -1,52 +1,10 @@
-const date = new Date();
-
-const month = {
-	monthNumber: date.getMonth() + 1,
-	longMonthName: date.toLocaleString('default', { month: 'long' }),
-	shortMonthName: date.toLocaleString('default', { month: 'short' }),
-	narrowMonthName: date.toLocaleString('default', { month: 'narrow' }),
-};
-
-const day = {
-	dayNumber: date.getDate(),
-	longDayName: date.toLocaleString('default', { weekday: 'long' }),
-	shortDayName: date.toLocaleString('default', { weekday: 'short' }),
-	narrowDayName: date.toLocaleString('default', { weekday: 'narrow' }),
-	dayOfWeekNumber: date.getDay() + 1,
-};
-
-const year = {
-	fullYear: date.getFullYear(),
-	shortYear: date.getYear() - 100,
-};
-
-function getWeekNumber(d) {
-	// Copy date so don't modify original
-	d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-	// Set to nearest Thursday: current date + 4 - current day number
-	// Make Sunday's day number 7
-	d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-	// Get first day of year
-	var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-	// Calculate full weeks to nearest Thursday
-	var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-	// Return array of year and week number
-	return [`Week: ` + weekNo];
-}
-
-console.log(getWeekNumber(date));
-
-const fullDate = {
-	fullDate1: date.toDateString(),
-	fullDate2: date.toLocaleDateString(),
-};
-
-const time = {
-	hours: date.getHours(),
-	minutes: date.getMinutes(),
-	seconds: date.getSeconds(),
-
-	currentTime() {
-		return `${this.hours}:${this.minutes}:${this.seconds}`;
-	},
-};
+const getByClass = (className) => document.getElementsByClassName(className);
+const getById = (id) => document.getElementById(id);
+const createElement = (element) => document.createElement(element);
+const appendChild = (parent, child) => parent.appendChild(child);
+const addClass = (element, className) => element.classList.add(className);
+const removeClass = (element, className) => element.classList.remove(className);
+const toggleClass = (element, className) => element.classList.toggle(className);
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+const textContent = (element, text) => (element.textContent = text);
